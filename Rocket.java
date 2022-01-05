c93343d3aa7b21178b3740cba8feb1a3896ffe72
umeshbn <umeshbneelaguri.xworkz@gmail.com>
2022-01-05 09:35:03 +0530

    method overloading

---
+++ Rocket.java
@@ -0,0 +1,39 @@
+class Rocket{
+
+	static void launching(){
+		System.out.println("torpedoes");
+	}
+	static void launching(int topmusic){
+		System.out.println("saturnv");
+	} 
+	static void launching(int topmusic2, String topmusic3){
+		System.out.println("spacerockets");
+	}
+	static void launching(String topmusic4, int topmusic5){
+		System.out.println("sattelites");
+	}
+	static void testing(){
+		System.out.println("launchingtesting");
+	}
+	static void testing(int jkll){
+		System.out.println("sattelitestesting");
+	} 
+	static void testing(int qwer, String tyui){
+		System.out.println("propellertesting");
+	}
+	static void testing(String opas, int dfgh){
+		System.out.println("spacetesting");
+	}
+	static void rescueoperations(){
+		System.out.println("china");
+	}
+	static void rescueoperations(int location){
+		System.out.println("usa");
+	} 
+	static void rescueoperations(int location2, String location3){
+		System.out.println("japan");
+	}
+	static void rescueoperations(String location4, int location5){
+		System.out.println("africa");
+	}
+}
---
+++ RocketTesting.java
@@ -0,0 +1,34 @@
+class RocketTesting{
+	public static void main(String []args){
+		
+		System.out.println("launching feature");
+		Rocket.launching();
+		int topmusic=13;
+		Rocket.launching(topmusic);
+		int topmusic2= 14; String topmusic3="to 150 speed";
+		Rocket.launching(topmusic2, topmusic3);
+		String topmusic4= "800 rpm"; int topmusic5= 40;
+		Rocket.launching(topmusic4, topmusic5);
+		System.out.println();
+		System.out.println("testing feature");
+		Rocket.testing();
+		int jkll=13;
+		Rocket.testing(jkll);
+		int qwer= 14; String tyui="to 150 speed";
+		Rocket.testing(qwer, tyui);
+		String opas= "800 rpm"; int dfgh= 40;
+		Rocket.testing(opas, dfgh);
+		System.out.println();
+		System.out.println("rescue operations feature");
+		Rocket.rescueoperations();
+		int location=13;
+		Rocket.rescueoperations(location);
+		int location2= 14; String location3="to 150 speed";
+		Rocket.rescueoperations(location2, location3);
+		String location4= "800 rpm"; int location5= 40;
+		Rocket.rescueoperations(location4, location5);
+		
+	
+	}
+
+}
